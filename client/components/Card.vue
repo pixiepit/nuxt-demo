bd348eea95ec8858147fa4432e45146edebd4d53
Ashlyn <lilyx13@tutanota.com>
2021-11-13 15:44:35 -0700

    optimized card

--- client/components/Card.vue
+++ client/components/Card.vue
@@ -1,25 +1,12 @@
 <template>
   <figure>
-    <nuxt-img :src="image" :alt="name" width="300" />
-    <figcaption>{{ bio }}</figcaption>
+    <nuxt-img :src="member.image" :alt="member.name" width="300" />
+    <figcaption>{{ member.bio }}</figcaption>
   </figure>
 </template>
 
 <script>
 export default {
-  props: {
-    image: {
-      type: String,
-      required: true
-    },
-    name: {
-      type: String,
-      required: true
-    },
-    bio: {
-      type: String,
-      required: true
-    }
-  }
+  props: ['member'],
 }
 </script> 
--- client/pages/team.vue
+++ client/pages/team.vue
@@ -2,12 +2,7 @@
   <div>
     <h1>Super Team</h1>
     <section class="container mx-auto flex flex-wrap justify-center gap-4">
-      <Card v-for="(member, index) in members" 
-        :key="index" 
-        :image="member.image" 
-        :name="member.name" 
-        :bio="member.bio" 
-      />
+      <Card v-for="member in members" :key="member.id" :member="member" />
     </section>
   </div>
 </template>
@@ -20,26 +15,26 @@ export default {
         {
           name: 'Andrea',
           image: '/assets/andrea-pic.jpg',
-          bio: 'Andrea is a cool person, apparently.'
+          bio: 'Andrea is a cool person, apparently.',
         },
         {
           name: 'Anthony',
           image: '/assets/anthony-pic.jpg',
-          bio: 'Super duper cool Anth.'
+          bio: 'Super duper cool Anth.',
         },
         {
           name: 'Italo',
           image: '/assets/italo-pic.jpg',
-          bio: 'Italo! Italo! Italo!'
-        }
-      ]
+          bio: 'Italo! Italo! Italo!',
+        },
+      ],
     }
-  }
+  },
 }
 </script>
 
 <style>
-  figcaption {
-    @apply text-center text-lg
-  }
+figcaption {
+  @apply text-center text-lg;
+}
 </style>
